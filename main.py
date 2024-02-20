c3d3d398f5b8dd8a7502ab1981e7207ae784e0a1
JungKH33 <melon345@yonsei.ac.kr>
2024-02-20 15:24:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from data import *
 from segmentation import *
+import os
 
 if __name__ == "__main__":
     input_nii_file = r"C:\projects\NeurophetSegBaseEngine\data\mask\irb82_0001_20230721_swi_synthseg.nii.gz"
     input_nii_file = r"C:\projects\SynthSeg-master\output\irb82_0001_20230721_swi_synthseg.nii.gz"
-    input_nii_file = r"C:\projects\SynthSeg-master\output\irb82_0002_20230720_swi_mag_synthseg.nii.gz"
+    input_nii_file = r"C:\database\dataset\mask_robust\irb82_0002_20230720_swi_mag_synthseg.nii.gz"
     output_nii_file = r'C:\projects\NeurophetSegBaseEngine\jung\test.nii'
+
     # merge_mask(input_nii_file, output_nii_file)
     img, data = load_nii(input_nii_file)
 
-    type = 3
+    type = 1
     if type == 1:
         data = merge_labels(data, labels=[3, 8, 42, 47], target_label=(1, 0))
         data = add_padding(data, labels= [1], padding_size = 1)
@@ -20,21 +22,20 @@ if __name__ == "__main__":
         data = add_padding(data, labels = [1])
 
     elif type == 3:
-        data = get_borders(data, labels =[3, 8, 42, 47], target_label= 1)
+        data = get_borders(data, labels = [3, 8, 42, 47], target_label= 1)
 
     else:
         data = get_borders(data, labels=[24, 3, 8, 42, 47])
         data = add_padding(data, labels=[1])
 
-
     save_nii(output_nii_file, data, img.affine)
 
 
-    input_directory = r"C:\database\dataset\mask_irb83"
-    output_directory = r'C:\projects\NeurophetSegBaseEngine\jung\temp'
-
     import sys
-    sys.exit()
+    #sys.exit()
+
+    input_directory = r"C:\database\dataset\mask_robust"
+    output_directory = r'C:\projects\NeurophetSegBaseEngine\jung\temp'
 
     for filename in os.listdir(input_directory):
         if filename.endswith(".nii.gz") or filename.endswith(".nii"):
@@ -43,4 +44,7 @@ if __name__ == "__main__":
             output_file_path = os.path.join(output_directory, filename)
 
             # Merge and save mask
-            merge_mask(input_file_path, output_file_path)
+            img, data = load_nii(input_file_path)
+            data = merge_labels(data, labels=[3, 8, 42, 47], target_label=(1, 0))
+            data = add_padding(data, labels=[1], padding_size=1)
+            save_nii(output_file_path, data, img.affine)
